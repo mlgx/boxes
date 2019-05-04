92e364689704acdc6127caba956eef7362927e85
Maxime Legaignoux <maxime.lgx@gmail.com>
2019-05-04 18:56:32 -0500

    Add Credo and Dialyxir.
    Signed-off-by: Maxime Legaignoux <maxime.lgx@gmail.com>

--- mix.exs
+++ mix.exs
@@ -33,15 +33,17 @@ defmodule Boxes.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.4"},
-      {:phoenix_pubsub, "~> 1.0"},
+      {:cowboy, "~> 1.0"},
+      {:credo, "~> 1.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: :dev, runtime: false},
+      {:gettext, "~> 0.11"},
       {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix, "~> 1.3.4"},
       {:plug, "1.5.0"},
-      {:cowboy, "~> 1.0"}
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 
